04caf7d0b24d9a85051ac2e03b4750adab016dfd
Lars <lars.scheme@gmail.com>
2016-02-15 16:56:38 +0100

    Added sample for switching to fullscreen mode.

---
+++ samples/fullscreen/README.md
@@ -0,0 +1,5 @@
+This sample switches to 640 x 840 fullscreen mode and displays a pink background. There is no interactivity, to quit the program use Alt+F4.
+
+For window creation this sample uses [Allen Dang's w32 library](https://github.com/AllenDang/w32). Direct3D needs a handle to the window it runs in so you need a method for setting this up. Other libraries that you can use include [SDL2](https://github.com/veandco/go-sdl2) and the [walk library](https://github.com/lxn/walk).
+
+If you simply build this sample with `go build` the resulting program will keep a console window open while running. Use the `build.bat` to build instead, it passes the flag `-H=windowsgui` to the linker which gets rid of the console window.
---
+++ samples/fullscreen/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/AllenDang/gform"
+	"github.com/AllenDang/w32"
+	"github.com/gonutz/d3d9"
+	"unsafe"
+)
+
+func main() {
+	gform.Init()
+
+	form := gform.NewForm(nil)
+	windowHandle := unsafe.Pointer(form.Handle())
+	form.Show()
+	form.OnClose().Bind(func(arg *gform.EventArg) {
+		w32.DestroyWindow(form.Handle())
+	})
+
+	check(d3d9.Init())
+	defer d3d9.Close()
+
+	d3d, err := d3d9.Create(d3d9.SDK_VERSION)
+	check(err)
+	defer d3d.Release()
+
+	device, _, err := d3d.CreateDevice(
+		d3d9.ADAPTER_DEFAULT,
+		d3d9.DEVTYPE_HAL,
+		windowHandle,
+		d3d9.CREATE_SOFTWARE_VERTEXPROCESSING,
+		d3d9.PRESENT_PARAMETERS{
+			Windowed:         false,
+			HDeviceWindow:    windowHandle,
+			SwapEffect:       d3d9.SWAPEFFECT_DISCARD,
+			BackBufferFormat: d3d9.FMT_X8R8G8B8,
+			BackBufferWidth:  640,
+			BackBufferHeight: 480,
+		},
+	)
+	check(err)
+	defer device.Release()
+
+	check(device.Clear(nil, d3d9.CLEAR_TARGET, d3d9.ColorRGB(255, 0, 255), 0, 1))
+	check(device.Present(nil, nil, nil, nil))
+
+	gform.RunMainLoop()
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
